66681075571acbfbe4ea6ca4dd0300af35c1b9b5
liuxinya <1164002431@qq.com>
2020-03-28 00:37:58 +0800

    add theme test

---
+++ test/precache-manifest.9ad6a52b2cd50536a071491f88cd2ac8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de5aa8c3f37654c6f7a45daa138108d0",
+    "url": "index.html"
+  },
+  {
+    "revision": "c73d535677a76deeb7e5",
+    "url": "static/css/2.081d97a7.chunk.css"
+  },
+  {
+    "revision": "77f980761833be912313",
+    "url": "static/css/main.23291248.chunk.css"
+  },
+  {
+    "revision": "c73d535677a76deeb7e5",
+    "url": "static/js/2.0c2424ef.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "static/js/2.0c2424ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77f980761833be912313",
+    "url": "static/js/main.be6b68f6.chunk.js"
+  },
+  {
+    "revision": "3fe803c877796c517216",
+    "url": "static/js/runtime-main.aeee4320.js"
+  }
+]);
